195f2fc3ad899fec405b405ac74abb2b82b8f105
szabotudor <szabo.tudor@gmail.com>
2023-11-14 01:05:27 +0100

    Testing new version

--- tests/basic_ecs.cpp
+++ tests/basic_ecs.cpp
@@ -26,7 +26,9 @@ struct TestInt {
 auto test_mgmecs(const uint32_t num_ents) {
     auto start = std::chrono::high_resolution_clock::now();
     mgm::MgmEcs ecs{};
-    auto ents = ecs.create(num_ents);
+    std::vector<mgm::EntityType> ents;
+    ents.resize(num_ents);
+    ecs.create(ents.begin(), ents.end());
     int i = 467831;
     for (const auto& e : ents) {
         ecs.emplace<TestInt>(e, i);
@@ -60,62 +62,33 @@ int main() {
 
     mgm::MgmEcs ecs{};
     std::cout << "\nTesting creating a number of entities" << std::endl;
-    const auto ents  = ecs.create(100);
+    std::vector<mgm::EntityType> ents;
+    ents.resize(100);
+    ecs.create(ents.begin(), ents.end());
     uint32_t i = 0;
     for (const auto& e : ents) {
-        ecs.get_or_emplace<uint32_t>(e, i++);
+        ecs.emplace<uint32_t>(e, i++);
     }
 
-    mgm::ComponentReference cr{};
+    ents.erase(ents.begin(), ents.begin() + 50);
+    ents.erase(ents.begin() + 13, ents.end());
+    ecs.destroy(ents.begin(), ents.end());
 
     std::cout << "\nTesting component references" << std::endl;
-    ecs.emplace<int>(0, 69);
-    const auto refs = ecs.get_all(0);
+    ecs.emplace<int>(1, 69);
+    const auto refs = ecs.get_all(1);
     for (const auto& ct : refs) {
-        if (ct.type_id == mgm::MgmEcs::type_id<int>)
+        if (ct.type_id == mgm::MgmEcs<>::type_id<int>)
             std::cout << "This is an int: " << ct.get<int>();
-        else if (ct.type_id == mgm::MgmEcs::type_id<uint32_t>)
+        else if (ct.type_id == mgm::MgmEcs<>::type_id<uint32_t>)
             std::cout << "This is a uint32_t: " << ct.get<uint32_t>();
         std::cout << std::endl;
     }
 
-    std::cout << "\nTesting destroying a number of entities" << std::endl;
-    const std::vector<mgm::Entity> ents_to_destroy{ents.begin() + 30, ents.begin() + 54};
-    ecs.destroy(ents_to_destroy);
-    for (const auto& e : ecs.all()) {
-        const auto& c = ecs.get<uint32_t>(e);
-        std::cout << c << ", ";
-    }
-    std::cout << std::endl;
-
-    std::cout << "\nTesting creating a number of entities less than available deleted entities" << std::endl;
-    auto destroyed_ents = ecs.create(20);
-    for (const auto& e : destroyed_ents) {
-        ecs.emplace<uint32_t>(e, i++);
-    }
-    for (const auto& e : ecs.all()) {
-        const auto* c = ecs.try_get<uint32_t>(e);
-        if (c != nullptr)
-            std::cout << *c << ", ";
-    }
-    std::cout << std::endl;
-
-    std::cout << "\nTesting creating a number of entities more than available entities" << std::endl;
-    destroyed_ents = ecs.create(5);
-    for (const auto& e : destroyed_ents) {
-        ecs.emplace<uint32_t>(e, i++);
-    }
-    for (const auto& e : ecs.all()) {
-        const auto* c = ecs.try_get<uint32_t>(e);
-        if (c != nullptr)
-            std::cout << *c << ", ";
-    }
-    std::cout << std::endl;
-
     std::cout << "\nCausing an intentional crash..." << std::endl;
     try {
-        std::cout << "Getting an int reference to an int component... " << refs[mgm::MgmEcs::type_id<int>].get<int>() << std::endl;
-        std::cout << "Getting an int reference to a uint32_t component... " << refs[mgm::MgmEcs::type_id<uint32_t>].get<int>() << std::endl;
+        std::cout << "Getting an int reference to an int component... " << refs[ecs.type_id<int>].get<int>() << std::endl;
+        std::cout << "Getting an int reference to a uint32_t component... " << refs[ecs.type_id<uint32_t>].get<int>() << std::endl;
     } catch (std::runtime_error err) {
         std::cout << "Caught an error: \n\t" << err.what() << std::endl;
     }
